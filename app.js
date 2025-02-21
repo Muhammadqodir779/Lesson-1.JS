bea0148b64aa7c8490df0cdf4f72640b2d02b822
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2025-02-21 22:11:24 +0500

    lesson-1 JS commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Foydalanuvchidan ism kiritishni so'raymiz
+let ism = prompt("Ismingizni kiriting:");
+
+// Kiritilgan ismni alert orqali chiqaramiz
+alert("Salom, " + ism + "! Xush kelibsiz!");
+
+// Konsolga ham xabar chiqaramiz
+console.log("Foydalanuvchi ismi: " + ism);
+
+let familya = prompt("Familyangizni kiriting:");
+
+alert("Yaxshi, "+ familya +"! Familyangiz qabul qilindi!");
+
+console.log("Foydalanuvchi familyasi: " + familya);
+
+let yosh = prompt("Yoshingizni kiriting:");
+
+alert("Zo'r, "+ yosh +" !Yoshingiz muvaffaqiyatli kiritildi!");
+
+console.log("Foydalanuvchi yoshi:" + yosh);
+
+let email = prompt("Emailingizni yozing:");
+
+alert("Yaxshi, "+ email +" !Emailingiz kiritildi!");
+
+console.log("Foydalanuvchi emaili:" + email);
+
+let qurilma = prompt("Qurilmangizni kiriting:");
+
+alert("Zo'r, "+ qurilma +" !Qurilma kiritildi!");
+
+console.log("Foydalanuvchi qurilmasi:" + qurilma);
+
+let manzil = prompt("Manzilingizni kiriting:");
+
+alert("Yaxshi, "+ manzil +" !Manzilingiz oson tarzda qabul qilindi!");
+
+console.log("Foydalanuvchi manzili:" + manzil);
